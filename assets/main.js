08c8a0fddff105bc4c99646513c8f65bc18035fc
tbxenia <tbxenia@yandex.ru>
2025-05-28 16:31:41 +0300

    Поиск

--- assets/main.js
+++ assets/main.js
@@ -6336,7 +6336,7 @@ searchInput.addEventListener('input', function() {
 			return response.text();
 		})
 		.then(data => {
-			searchCategory.innerHTML(data);
+			searchCategory.innerHTML = data;
 		})
 		.catch((error) => {
 			console.log('Ошибка загрузки файла');
